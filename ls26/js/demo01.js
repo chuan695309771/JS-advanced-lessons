e720f86e2dd7c735c9163c8d0f1b3825c4c0d9d8
chuan695309771 <695309771@qq.com>
2018-06-03 19:50:59 +0800

    十三周第二节

---
+++ ls26/js/demo01.js
@@ -0,0 +1,12 @@
+xhr.onreadystatechange = function () {
+    //console.log(xhr.readyState,xhr.status);
+    if (xhr.readyState == 4) {
+        //表示服务器的相应代码是200；正确返回了数据
+        if (xhr.status == 200) {
+            var message = xhr.responseText;
+            console.log(message);
+        }
+    }
+};
+xhr.open("get", "http://127.0.0.1:8080?getInfo=MyGetInformation", true);
+xhr.send();
